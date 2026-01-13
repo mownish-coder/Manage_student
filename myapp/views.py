8e7006721f402b1bb321a3fad65d5f92925b17e9
mownish-coder <mownishwaran@gmail.com>
2026-01-13 12:21:36 +0530

    Add home view to render home.html

--- myapp/views.py
+++ myapp/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
-
+def home(request):
+  return render('home.html')
 # Create your views here.
 # This file is intentionally left blank for future view implementations.
