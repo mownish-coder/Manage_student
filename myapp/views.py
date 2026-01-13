dbc372c6cb81093d94090fdfa15fc9503a8c6f21
mownish-coder <mownishwaran@gmail.com>
2026-01-13 14:41:01 +0530

    created home.html

--- myapp/views.py
+++ myapp/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
 def home(request):
-  return render('home.html')
+  return render(request, 'home.html')
 # Create your views here.
 # This file is intentionally left blank for future view implementations.
